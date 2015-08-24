e17328af40be9164d3c16d023c2b6692ab8156ca
Allen <a1b2c3d42003hongkong@hotmail.com>
2015-08-24 16:10:37 +0800

    update to multi-interface

--- DDNSupdater.js
+++ DDNSupdater.js
@@ -5,23 +5,36 @@ var file = fs.createWriteStream('./update.log',{flags: 'a', mode:0644});
 var dns = [];
 
 (function(){
-	var dnsModules = fs.readdirSync('./lib-enable');
+	var dnsModules = fs.readdirSync('./lib');
+	var tmp;
 	for(var i=0;i<dnsModules.length; i++){
-		if(dnsModules[i].match(/\.js$/)){
-			dns.push(require('./lib-enable/'+dnsModules[i]));
+		if(dnsModules[i].match(/\.js$/) && (tmp = require('./lib/'+dnsModules[i]))){
+			dns.push(tmp);
 		}
 	}
 })();
 
-var ip = 0;
+if(dns.length == 0){
+	console.error('No Configuation found.');
+	process.exit(1);
+}
+
+var ip = {};
+conf.if.forEach(function(interface){
+	ip[interface]=0;
+});
+
 var isIPChanged = (function(){
 	var result = false;
 	return function(){
 		var ifList = os.networkInterfaces();
-		for(var i = 0; i < ifList['p4p1'].length; i++){
-			if('IPv4' === ifList['p4p1'][i].family && ifList['p4p1'][i].internal === false){
-				if(result = (ip != ifList['p4p1'][i].address)){
-					ip = ifList['p4p1'][i].address;
+		for(var x in ip){
+			for(var i = 0; !!ifList[x] && i < ifList[x].length; i++){
+				if('IPv4' === ifList[x][i].family && ifList[x][i].internal === false){
+					if(ip[x] != ifList[x][i].address){
+						ip[x] = ifList[x][i].address;
+						result = true;
+					}
 				}
 			}
 		}
@@ -29,19 +42,30 @@ var isIPChanged = (function(){
 	}
 })();
 
-var log = function(result){
-	try{
-		file.write('['+new Date().toISOString()+'] "'+result.domain+'" is updated to "'+result.ip+'"'+"\n");
-	}catch(e){
-		console.log(e);
+var log = (function(){
+	switch(conf.log){
+		case 'json':
+			return function(result){
+				try{
+					file.write('{time:'+Date.now()+',domain:"'+result.domain+'",ip:"'+result.ip+'"}'+"\n");
+				}catch(e){
+					console.log(e);
+				}
+				console.log('{time:'+Date.now()+',domain:"'+result.domain+'",ip:"'+result.ip+'"}');
+			};
+		default:
+			return function(result){
+				try{
+					file.write('['+new Date().toISOString()+'] "'+result.domain+'" is updated to "'+result.ip+'"'+"\n");
+				}catch(e){
+					console.log(e);
+				}
+				console.log('['+new Date().toISOString()+'] "'+result.domain+'" is updated to "'+result.ip+'"');
+			};
 	}
-	console.log('['+new Date().toISOString()+'] "'+result.domain+'" is updated to "'+result.ip+'"');
-}
+})();
 
 var updateIP = function(){
-//	for(var i=0;i<dns.length;i++){
-//		dns[i](log, ip);
-//	}
 	dns.forEach(function(module){module(log, ip)});
 }
 
